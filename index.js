d3b7beb7819770fd32fd1b611ff685b49889c8d0
bbouley <b.bouley@hotmail.co.uk>
2015-07-13 15:42:53 -0600

    solution to problem 4

--- index.js
+++ index.js
@@ -12,11 +12,24 @@ function isEqual(x,y){
 console.log(isEqual(2,3))
 
 function discountPercentage (original, discount){
-  if (discount <= original && discount != 0){
+  if (discount <= 100 && discount != 0){
   return '$' + (discount/original) * 100
 } else {
   return 'That can\'t be right!!!'
 }
 }
 
-console.log (discountPercentage(100,46))
+console.log (discountPercentage(40,1))
+
+function stringCapitalize (str){
+  var strarray = str.split(' ');
+  var newarray = []
+  for (var i = 0; i < strarray.length; i++){
+  newarray.push(strarray[i].charAt(0).toUpperCase() + strarray[i].slice(1));
+  };
+  var strarray = newarray.join(' ')
+  return strarray
+ };
+
+var str = 'this is a string'
+console.log(stringCapitalize(str))
